d5f551723b3e421b648fe3085117789c755016de
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-07-16 02:05:02 -0400

    add functionality to store _next_state

--- game_of_life/src/components/Cell.js
+++ game_of_life/src/components/Cell.js
@@ -1,16 +1,27 @@
 export default class Cell {
 	constructor(active = false){
 		this._active = active;
-		this._gen = 1;
+		this._gen = 0;
+		this._next_state = active
 	}
 
 	toggle_active(){
-		this._gen = this.active ? 0 : 1
-		this._active = !this.active
+		if (this._active){
+			this._gen = 0;
+		}
+		this._active = !this._active;
 	}
 
 	get is_active(){
-		return this._active
+		return this._active;
+	}
+
+	set_next_state(active){
+		this._next_state = active;
+	}
+
+	get next_state(){
+		return this._next_state
 	}
 
 	inc_gen(inc = 1){
