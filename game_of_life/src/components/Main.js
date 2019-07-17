b599d89dd0ea632cd8fb0813ec98f35b1b5d6d90
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-07-17 17:32:22 -0400

    Add semicolons

--- game_of_life/src/components/Main.js
+++ game_of_life/src/components/Main.js
@@ -1,42 +1,42 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect} from 'react';
 
 import Cell from './Cell';
 import {make_grid_of, update_cell_states, buffer, draw, game_of_life, random_config} from './helpers';
 
-function Main(props) {
+function Main(props){
 	const [grid, set_grid] = useState();
 	const [world_gen, set_world_gen] = useState(0);
 
-	const [main_canvas, set_main_canvas] = useState()
-	const [rows, set_rows] = useState()
-	const [cols, set_cols] = useState()
-	const [resolution] = useState(5)
+	const [main_canvas, set_main_canvas] = useState();
+	const [rows, set_rows] = useState();
+	const [cols, set_cols] = useState();
+	const [resolution] = useState(5);
 
-	const [anim_loop, set_anim_loop] = useState()
-	const [is_animating, set_is_animating] = useState(false)
+	const [anim_loop, set_anim_loop] = useState();
+	const [is_animating, set_is_animating] = useState(false);
 	
 	useEffect(() => {
-		const canvas = document.getElementById('canvas')
+		const canvas = document.getElementById('canvas');
 		let rows = canvas.height/resolution; 
 		let cols = canvas.width/resolution;
-		let new_grid = make_grid_of(Cell, rows, cols)
-		random_config(new_grid)
+		let new_grid = make_grid_of(Cell, rows, cols);
+		random_config(new_grid);
 
-		set_main_canvas(canvas)
-		set_rows(rows)
-		set_cols(cols)
-		set_grid(new_grid)
-		update_cell_states(grid)
-		draw(grid, canvas.getContext('2d'), rows, cols, resolution)
-	},[resolution])
+		set_main_canvas(canvas);
+		set_rows(rows);
+		set_cols(cols);
+		set_grid(new_grid);
+		update_cell_states(grid);
+		draw(grid, canvas.getContext('2d'), rows, cols, resolution);
+	},[resolution]);
 
 	useEffect(() => {
-		const canvas = document.getElementById('canvas')
-		game_of_life(grid)
-		update_cell_states(grid)
-		let buffr = buffer(grid, canvas, rows, cols, resolution)
-		canvas.getContext('2d').drawImage(buffr, 0, 0)
-	},[grid,world_gen])
+		const canvas = document.getElementById('canvas');
+		game_of_life(grid);
+		update_cell_states(grid);
+		let buffr = buffer(grid, canvas, rows, cols, resolution);
+		canvas.getContext('2d').drawImage(buffr, 0, 0);
+	},[grid,world_gen]);
 
 	return(
 		<div>
@@ -49,13 +49,13 @@ function Main(props) {
 				if (!is_animating){
 					const loop = setInterval(() => {
 						set_world_gen(prev_gen => prev_gen + 1)
-					}, 100)
-					set_anim_loop(loop)
-					set_is_animating(true)
+					}, 100);
+					set_anim_loop(loop);
+					set_is_animating(true);
 				}
 				else {
-					clearInterval(anim_loop)
-					set_is_animating(false)
+					clearInterval(anim_loop);
+					set_is_animating(false);
 				}
 			}}>{is_animating ? 'Stop': 'Animate'}</button>
 			
@@ -64,13 +64,13 @@ function Main(props) {
 			}}>Next</button>
 
 			<button id = 'reset_btn' disabled = {is_animating ? true : false} onClick = {() => {
-				set_world_gen(0)
-				random_config(grid)
-				update_cell_states(grid)
-				draw(grid, main_canvas.getContext('2d'), rows, cols, resolution)
+				set_world_gen(0);
+				random_config(grid);
+				update_cell_states(grid);
+				draw(grid, main_canvas.getContext('2d'), rows, cols, resolution);
 			}}>Reset</button>
 		</div>
 	)
 }
 
-export default Main
+export default Main;
