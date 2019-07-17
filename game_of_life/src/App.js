c551ffed3318ca61807320f32fab6057c46ee64d
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-07-16 23:18:18 -0400

    Add structural divs

--- game_of_life/src/App.js
+++ game_of_life/src/App.js
@@ -7,7 +7,20 @@ function App() {
 	return (
 		<div className="App">
 			<div className="App-header">
-				<Main/>
+				<div id = 'main'>
+					<Main/>
+				</div>
+				<div id = "rules">
+					<p>For each cell, it counts that cell's eight neighbors (up, down, left, right, and diagonals), and then act on that result.</p>
+					<ul>
+						<li>
+							If the cell is alive and has 2 or 3 neighbors, then it remains alive. Else it dies.
+						</li>
+						<li>
+							If the cell is dead and has exactly 3 neighbors, then it comes to life. Else if remains dead.
+						</li>
+					</ul>
+				</div>
 			</div>
 		</div>
 	);
